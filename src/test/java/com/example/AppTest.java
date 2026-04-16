8bd4e40acd3a2414f88bf5ca711c71833c4480e2
abishar605 <abisha.r2023@vitstudent.ac.in>
2026-04-16 16:41:27 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,10 +1,15 @@
 package com.example;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+
 public class AppTest {
     @Test
     public void testPrime() {
         assertTrue(App.isPrime(7));
+        assertTrue(App.isPrime(11));
         assertFalse(App.isPrime(4));
+        assertFalse(App.isPrime(1));
     }
 }
